ba6b6344773f41bc8be99c456a364e0891b4c8d7
Yishan-Qiu <37771437+zhaojianing@users.noreply.github.com>
2019-03-27 00:55:00 +0800

    init: servers

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const Router = require('koa-router')
+
+const ArticleController = require('../controllers/article')
+
+
+const router = new Router({
+  prefix: '/api/v1'
+})
+
+/**
+* 文章接口
+*/
+// 创建文章接口（路由）
+router.post('/article', ArticleController.create);
+// 获取文章详情接口（路由）
+router.get('/article/:id', ArticleController.detail);
+
+// const router = require('koa-router')()
+// router.get('/', async (ctx, next) => {
+//   await ctx.render('index', {
+//     title: 'Hello Koa 2!'
+//   })
+// })
+
+// router.get('/string', async (ctx, next) => {
+//   ctx.body = 'koa2 string'
+// })
+
+// router.get('/json', async (ctx, next) => {
+//   ctx.body = {
+//     title: 'koa2 json'
+//   }
+// })
+
+module.exports = router
